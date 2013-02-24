0295e0a271d8ffa081bbddf85fbeb2f669aa850e
douglasmccuen <douglasmccuen@gmail.com>
2013-02-24 10:45:10 -0700

    empty checkin to test git config

---
+++ mobile/RegistrationViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.h
+//  mobile
+//
+//  Created by Douglas McCuen on 2/24/13.
+//  Copyright (c) 2013 Douglas McCuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationViewController : UIViewController
+
+@end
