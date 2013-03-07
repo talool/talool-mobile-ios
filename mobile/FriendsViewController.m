76812690f176fd3fc1ba822d04df41aaab797ec4
douglasmccuen <douglasmccuen@gmail.com>
2013-03-06 20:14:12 -0700

    use the transport object

--- mobile/FriendsViewController.m
+++ mobile/FriendsViewController.m
@@ -29,7 +29,7 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)setCustomer:(Customer *)newCustomer {
+- (void)setCustomer:(ttCustomer *)newCustomer {
     if (newCustomer != customer) {
         customer = newCustomer;
     }
