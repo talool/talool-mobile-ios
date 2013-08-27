98c4c4d557be905139b3c74a31fe91f9b4de6e4a
Doug McCuen <doug@talool.com>
2013-08-27 10:12:31 -0600

    * quiet the logging noise on iOS7 preview

--- mobile/main.m
+++ mobile/main.m
@@ -10,9 +10,39 @@
 
 #import "AppDelegate.h"
 
+/* Start of fix for iOS7 log noise 
+ https://devforums.apple.com/thread/197966?start=50&tstart=0
+ */
+
+typedef int (*PYStdWriter)(void *, const char *, int);
+static PYStdWriter _oldStdWrite;
+
+int __pyStderrWrite(void *inFD, const char *buffer, int size)
+{
+    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
+        return 0;
+    }
+    return _oldStdWrite(inFD, buffer, size);
+}
+
+int main(int argc, char *argv[])
+{
+    _oldStdWrite = stderr->_write;
+    stderr->_write = __pyStderrWrite;
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
+
+/* end of fix for iOS7 log noise
+ https://devforums.apple.com/thread/197966?start=50&tstart=0
+ */
+
+/* original code
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+*/
