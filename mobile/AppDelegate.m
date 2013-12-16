6c9a764288541ff6a823cb2d8a37ee53c941317e
Doug McCuen <doug@talool.com>
2013-12-16 13:44:54 -0700

    revert

--- mobile/AppDelegate.m
+++ mobile/AppDelegate.m
@@ -42,9 +42,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
-#warning @"Environment set to dev"
-    [[TaloolFrameworkHelper sharedInstance] setEnvironment:EnvironmentTypeDevelopment];
     
     //For now, register for all types of notifications
     [[UIApplication sharedApplication] registerForRemoteNotificationTypes:
@@ -250,9 +247,7 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    
-    
-    [[UIApplication sharedApplication] performSelector:@selector(_performMemoryWarning)];
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
